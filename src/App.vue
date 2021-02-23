aab8de8dea62547c1b2afe950d887ee419f0c2a5
Austin Chen <akrolsmir@gmail.com>
2021-02-23 07:56:12 -0800

    Play with Tailwind styling and Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <router-view></router-view>
+  <div class="center-column">
+    <div class="px-8">
+      <h1>Arpista Editing</h1>
+    </div>
+    <Navbar />
+    <router-view class="content-page"></router-view>
+  </div>
 </template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="navbar" class="flex flex-row px-8 space-x-6 py-2">
+    <div class="text-gray-100" v-for="(link, title) in links">
+      <router-link :to="link">{{ title }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const links = {
+  Home: '',
+  'Contact Me': '',
+  Qualifications: '',
+  Services: '',
+  'Useful Links': '',
+}
+</script>
+
+<style scoped>
+#navbar {
+  background-color: #00127a;
+}
+</style>
